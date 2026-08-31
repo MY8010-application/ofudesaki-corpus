d64df386c667adcdc1bfec8da70fb01148752b8e
MY8010-application <ch.mp.masa@gmail.com>
2026-08-31 10:59:18 +0900

    Add service worker for caching files

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "ofudesaki-corpus-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./corpus.json"
+];
+
+self.addEventListener("install", event => {
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(FILES_TO_CACHE))
+
+);
+
+});
+
+self.addEventListener("fetch", event => {
+
+event.respondWith(
+
+caches.match(event.request)
+.then(response => {
+
+return response || fetch(event.request);
+
+})
+
+);
+
+});
